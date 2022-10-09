17e7defd30e6b07f030ce0e1c787704123dd708f
lmartin23 <lmaartin997@gmail.com>
2022-10-09 11:13:32 -0300

    commit estructura proyecto

---
+++ ProyectoGrupo1/src/main/java/com/proyecto/grupo1/ProyectoGrupo1/ProyectoGrupo1Application.java
@@ -0,0 +1,13 @@
+package com.proyecto.grupo1.ProyectoGrupo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGrupo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGrupo1Application.class, args);
+	}
+
+}
