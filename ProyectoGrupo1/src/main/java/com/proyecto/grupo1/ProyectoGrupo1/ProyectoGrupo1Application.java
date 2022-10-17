df7834d28f4c05a2fc4b0abb7d6eb9c7630cd695
nch8 <chochein96@gmail.com>
2022-10-17 20:51:52 -0300

    Update ProyectoGrupo1Application.java

--- ProyectoGrupo1/src/main/java/com/proyecto/grupo1/ProyectoGrupo1/ProyectoGrupo1Application.java
+++ ProyectoGrupo1/src/main/java/com/proyecto/grupo1/ProyectoGrupo1/ProyectoGrupo1Application.java
@@ -12,14 +12,24 @@ public class ProyectoGrupo1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoGrupo1Application.class, args);
 	}
-/*
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("/**").allowedMethods("GET", "POST","PUT", "DELETE");;
+				registry.addMapping("/**").allowedHeaders("Access-Control-Allow-Origin",
+					"*",
+					"Access-Control-Allow-Methods",
+					"POST, GET, OPTIONS, PUT, DELETE",
+					"Access-Control-Allow-Headers",
+					"Origin, X-Requested-With, Content-Type, Accept")
+				.allowedOrigins("*")
+				.allowedMethods("*");
+
+				//registry.addMapping("/**").allowedOrigins("/**").allowedMethods("GET", "POST","PUT", "DELETE");;
 			}
 		};
-	}*/
+	}
+
 }
